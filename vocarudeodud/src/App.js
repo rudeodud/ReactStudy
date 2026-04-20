246dd80acb21f2544a3408bda88aad7b20afdb3f
Gyeongdaeyoung <86515840+rudeodud@users.noreply.github.com>
2026-04-20 15:33:04 +0900

    202604020_1

--- vocarudeodud/src/App.js
+++ vocarudeodud/src/App.js
@@ -3,9 +3,10 @@ import Hellow from './components/Hellow'
 function App() {
   return (
     <div className="App">
-      <Hellow />
-      <Hellow />
-      <Hellow />
+      <h3>props : properties</h3>
+      <Hellow age={10}/>
+      <Hellow age={20}/>
+      <Hellow age={30}/>
     </div>
   );
 } 
--- vocarudeodud/src/components/Hellow.jsx
+++ vocarudeodud/src/components/Hellow.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hellow(){
-    //let name = "Mike"
+export default function Hellow({age}){
     const[name, setName] = useState('Mike');
+    const msg = age > 19 ? "성인입니다" : "미성년자입니다";
     return (
         <div>
-            <h2>{name}</h2>
+            <h2>{name}({age}) : {msg}</h2>
+            <UserName name={name} />
             <button onClick={()=>{
-                setName(name ==="Mike" ? "Jane" : "Mike")
+                setName(name ==="Mike" ? "jane" : "Mike")
             }}>changeName</button>
         </div>
     );}
