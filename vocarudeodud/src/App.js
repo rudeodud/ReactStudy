5cdc0d1db195ef84ab285caba71e3421a4ae615a
Gyeongdaeyoung <86515840+rudeodud@users.noreply.github.com>
2026-05-07 09:23:01 +0900

    20260507_2

--- vocarudeodud/src/App.js
+++ vocarudeodud/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Hellow from './components/Hellow'
+import Header from './components/Header.jsx';
+
 function App() {
   return (
-    <div className="App">
-      <h3>props : properties</h3>
-      <Hellow age={10}/>
-      <Hellow age={20}/>
-      <Hellow age={30}/>
-    </div>
+  <div className="App">
+    <Header />
+  </div>
   );
 } 
 
--- vocarudeodud/src/components/Header.jsx
+++ vocarudeodud/src/components/Header.jsx
@@ -4,6 +4,14 @@ export default function Header(){
             <h1>
                 <a>토익 영단어(고급)</a>
             </h1>
+            <div className="menu">
+                <a href="#x" className="link">
+                    단어추가
+                </a>
+                <a href="#x" className="link">
+                    Day 추가
+                </a>
+            </div>
         </div>
     )
 }
